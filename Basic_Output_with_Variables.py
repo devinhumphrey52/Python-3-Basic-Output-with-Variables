3b6f90e92d50c369aaf2689de938706399307f9f
Devin Humphrey <60116652+devinhumphrey52@users.noreply.github.com>
2020-02-04 16:03:49 -0600

    Add files via upload

---
+++ Basic_Output_with_Variables.py
@@ -0,0 +1,19 @@
+userNum = int(input('Enter integer: \n'))
+
+print("You entered:", userNum)
+
+print (userNum,"squared is", end = " ")
+
+print(userNum * userNum)
+
+print("And", userNum, "cubed is", userNum * userNum *userNum, '!!')
+
+userNum2 = int(input('Enter another integer: \n'))
+
+print('4 +', userNum2, 'is', end =" ")
+
+print(4 + userNum2)
+
+print('4 *', userNum2, 'is', end=" ")
+
+print(4 * userNum2)
